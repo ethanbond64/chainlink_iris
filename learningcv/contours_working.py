1f79828c0a8387d632af58bfb277e8e0a55713d9
ethanbond64 <ethanbond39@gmail.com>
2021-11-14 23:11:30 -0700

    import cleanup

--- learningcv/contours_working.py
+++ learningcv/contours_working.py
@@ -1,9 +1,9 @@
-import cv2
-
+# import cv2
+from cv2 import imread, GaussianBlur, Canny, findContours, RETR_TREE, CHAIN_APPROX_SIMPLE
 def img_to_bin(img):
-    blurred = cv2.GaussianBlur(img, (5, 5), 0) # Blur
-    canny = cv2.Canny(blurred, 30, 150) # Canny
-    contours, _ = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    blurred = GaussianBlur(img, (5, 5), 0) # Blur
+    canny = Canny(blurred, 30, 150) # Canny
+    contours, _ = findContours(canny,RETR_TREE,CHAIN_APPROX_SIMPLE)
     contours = contours[0::len(contours)//25]
     bin_rep = ""
     for c in contours:
@@ -16,7 +16,7 @@ def img_to_bin(img):
 
 
 # img = cv2.imread("base6.png")
-img = cv2.imread("base7.png")
+img = imread("base7.png")
 base6 = "1100100011110101110010000"
 base7 = "1100100011111100010101110"
 
