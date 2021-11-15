d53d1e12acbbf1aa2bde32db74e0c121d9bb8dcb
ethanbond64 <ethanbond39@gmail.com>
2021-11-14 22:58:50 -0700

    getting closer

--- learningcv/contours2.py
+++ learningcv/contours2.py
@@ -16,11 +16,30 @@ cv2.waitKey()
 contours, _ = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 contours = contours[0::4]
 print('Cs')
+print(img.shape)
+bin_rep = ""
+
+# max_height = np.max(c[::, 3])
+# nearest = max_height * 1.4
+
+# contours.sort(key=lambda r: [int(nearest * round(float(r[1]) / nearest)), r[0]])
+
+# contours = cnts.sort_contours(contours, method="top-to-bottom")[0]
+print(len(contours))
 for c in contours:
     # print(c)
-    print(c[0])
-    img = cv2.circle(img, c[0][0], 5, (0, 0, 255), 2)
-    
+    print(img[c[0][0][0]+20][c[0][0][1]+20],img[c[0][0][0]+20][c[0][0][1]+20][0] != 255)
+    img = cv2.circle(img, (c[0][0][0]+20,c[0][0][1]+20), 5, (0, 0, 255), 2)
+    if img[c[0][0][0]+20][c[0][0][1]+20][0] != 255:
+        bin_rep += "1"
+    else:
+        bin_rep += "0"
+    cv2.imshow('Original Im',img)
+    cv2.waitKey()  
+
+bin_rep = bin_rep[::-1]
+print(bin_rep)
+print(bin_rep == "1100100011110101110010000")
     # print(img[c[0]])
 cv2.imshow('Original Im',img)
 cv2.waitKey()  
