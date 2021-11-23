ff15bf9be500148e706888608152dbb84063150f
ethanbond64 <ethanbond39@gmail.com>
2021-11-22 23:44:57 -0700

    stream in by event working

--- backend/server/blueprints/main/views.py
+++ backend/server/blueprints/main/views.py
@@ -4,10 +4,8 @@ from flask_cors import CORS
 import datetime
 
 
-
 main = Blueprint('main', __name__,template_folder='templates')
 CORS(main,origins="http://localhost:3000")
-
 
 # GET ENDPOINTS
 @main.route('/test',methods=['GET'])
@@ -28,10 +26,9 @@ def create_entry():
     if request.form.get("name") is not None:
         event = Event(
                 name=request.form.get("name"),
-                expiration=datetime.datetime.now()+datetime.timedelta(days=5))
+                expiration=read_js_date(request.form.get("expiration_date")))
 
         event.save()
-        
 
         resp['success'] = True
     return make_response(jsonify(resp),200)
@@ -58,3 +55,60 @@ def get_entries(event):
     resp = {"entries":[entry.json() for entry in entries]}
 
     return make_response(jsonify(resp),200)
+
+
+from flask import render_template
+from server.blueprints.main.models import Event
+from server.utils.extensions import socketio
+from flask_socketio import emit
+# from server.blueprints.main.views import main
+from server.blueprints.main.time_handler import img_to_bin
+from server.blueprints.main.data_manager import save_event_record
+
+from PIL import Image
+from io import BytesIO 
+import base64
+import numpy as np
+
+# Time authenticator route
+@main.route('/timebox',methods=['GET'])
+def view_timebox():
+    return render_template('timebox.html')
+
+# Video frame route
+@main.route('/stream/<event_id>',methods=['GET'])
+def stream_video(event_id):
+    return render_template('index.html',event_id=event_id)
+
+
+@socketio.on('VideoStreamIn', namespace='/iris')
+def stream_in(data):
+    print("VIDEO STREAM RECIEVED")
+    print(data)
+    
+    data_url = data["DataURL"]
+    event_id = data["EventID"]
+
+    # get event
+    event = Event.query.filter(Event.id==event_id).first()
+
+    if event is None :
+        # TODO handle errors via socketIO
+        emit('VideoStreamReceived', {'Valid': False}, namespace='/iris')
+    # Process and save data
+    img = stringToImage(data_url.split(",")[1])
+    save_event_record(img,event)
+ 
+    emit('VideoStreamReceived', {'Valid': True}, namespace='/iris')
+
+@socketio.on('connect', namespace='/iris')
+def test_connect():
+    print("SOCKETIO CONNECTED")
+
+def stringToImage(base64_string):
+    imgdata = base64.b64decode(base64_string)
+    return np.array(Image.open(BytesIO(imgdata)))
+
+
+def read_js_date(datestring):
+    return datetime.datetime.strptime(datestring, "%a, %d %b %Y %H:%M:%S %Z")
