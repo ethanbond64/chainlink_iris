ffdaebe9ac230764cbbea31c4717d4781cbd177d
ethanbond64 <ethanbond39@gmail.com>
2021-11-24 22:46:59 -0600

    beginning main ui page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,24 @@ function App() {
           </div>
         </li>
       </ul>
+
+      <div className={`grid grid-cols-3 gap-4`}>
+        <div className={`relative mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>
+          <div className={`absolute rounded-t-lg inset-x-0 top-0 p-2.5 bg-indigo-900`}>
+            header
+          </div>
+          <div>
+            Description
+          </div>
+          <div class={`absolute rounded-b-lg inset-x-0 bottom-0 p-2.5 bg-white h-14`}>
+            Buttons
+          </div>
+        </div>
+        <div className={`mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>2</div>
+        <div className={`mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>3</div>
+        <div className={`mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>4</div>
+      </div>
+
       <footer className={`text-center`}>
       </footer>
     </div>
