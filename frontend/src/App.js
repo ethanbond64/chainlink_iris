8977959c44204170beadc506f89641eba73eddcc
ethanbond64 <ethanbond39@gmail.com>
2021-11-24 23:04:40 -0600

    split card class, 2 links working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,28 @@
 import React, { useState, useEffect } from "react";
+import EventCard from "./EventCard";
 
 function App() {
+
+  let cards = [1,2,3,4];
+
+
   return (
     <div>
       {/* <Navbar tab="events" /> */}
       <ul className={`flex mb-6 mt-4`}>
         <li className={`mr-3`}>
-          <div className={`inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white`} href="/">
+          <div className={`inline-block border rounded py-1 px-3 bg-indigo-900 text-white`} href="/">
             New event
           </div>
         </li>
       </ul>
 
       <div className={`grid grid-cols-3 gap-4`}>
-        <div className={`relative mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>
-          <div className={`absolute rounded-t-lg inset-x-0 top-0 p-2.5 bg-indigo-900`}>
-            header
-          </div>
-          <div>
-            Description
-          </div>
-          <div class={`absolute rounded-b-lg inset-x-0 bottom-0 p-2.5 bg-white h-14`}>
-            Buttons
-          </div>
-        </div>
-        <div className={`mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>2</div>
-        <div className={`mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>3</div>
-        <div className={`mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>4</div>
+        {
+          cards.map(() => 
+            <EventCard name={`Header Prop`} description={`a paragraph`} id={1} />
+          )
+        }
       </div>
 
       <footer className={`text-center`}>
---
+++ frontend/src/EventCard.js
@@ -0,0 +1,31 @@
+
+
+function EventCard(props) {
+
+
+
+
+    return (
+        <div className={`relative mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>
+            <div className={`absolute rounded-t-lg inset-x-0 top-0 p-2.5 bg-indigo-900 text-white font-bold`}>
+                {props.name}
+            </div>
+            {props.description}
+            <div class={`absolute rounded-b-lg inset-x-0 bottom-0 p-2.5 bg-white h-14`}>
+                <div className={`grid grid-cols-3 gap-4`}>
+                    <a className={`inline-block border rounded py-1 px-3 bg-indigo-900 text-white`} href={`localhost:8000/stream/${props.id}`}>
+                        Stream
+                    </a>
+                    <a className={`inline-block border rounded py-1 px-3 bg-indigo-900 text-white`} href={`localhost:8000/timebox`}>
+                        Time Auth
+                    </a>
+                    <div className={`inline-block border rounded py-1 px-3 bg-blue-400 text-white`} >
+                        Contracts
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EventCard;
