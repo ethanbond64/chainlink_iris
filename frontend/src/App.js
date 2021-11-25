45cd17a9fce749accd57f517d5ba084c172f1a69
ethanbond64 <ethanbond39@gmail.com>
2021-11-25 10:16:46 -0600

    load the real events on main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,31 @@ import EventCard from "./EventCard";
 
 function App() {
 
-  let cards = [1,2,3,4];
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/get/events", {
+      // mode: 'no-cors',
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    }).then(response => {
+      if (response.ok) {
+        response.json().then(json => {
+          console.log(json.events);
+          setEvents(json.events);
+        });
+      }
+    });
+  }, []);
 
   return (
     <div>
       <div className={`grid grid-cols-3 gap-4`}>
         {
-          cards.map(() => 
-            <EventCard name={`Header Prop`} description={`a paragraph`} id={1} />
+          events.map((event) => 
+            <EventCard name={event.name} description={event.about} id={event.id} />
           )
         }
       </div>
