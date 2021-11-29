10d1ad51724026f65cc2029005224299dceac6c1
ethanbond64 <ethanbond39@gmail.com>
2021-11-28 17:58:04 -0700

    cleaner UI

--- frontend/src/EventCard.js
+++ frontend/src/EventCard.js
@@ -4,11 +4,14 @@ function EventCard(props) {
 
 
     return (
-        <div className={`relative mb-6 p-2.5 rounded shadow-md h-60 bg-gray-100`}>
+        <div className={`relative mb-6 p-2.5 rounded shadow-md h-60 bg-blue-100`}>
             <div className={`absolute rounded-t-lg inset-x-0 top-0 p-2.5 bg-indigo-900 text-white font-bold`}>
                 {props.name}
             </div>
-            {props.description}
+            <br/>
+            <div className={`mt-6`} >
+                {props.description}
+            </div>
             <div class={`absolute rounded-b-lg inset-x-0 bottom-0 p-2.5 bg-white h-14`}>
                 <div className={`grid grid-cols-3 gap-4`}>
                     <a className={`inline-block border rounded py-1 px-3 bg-blue-400 text-white`} href={`localhost:8000/stream/${props.id}`}>
@@ -17,9 +20,9 @@ function EventCard(props) {
                     <a className={`inline-block border rounded py-1 px-3 bg-blue-400 text-white`} href={`localhost:8000/timebox`}>
                         Time Auth
                     </a>
-                    <div className={`inline-block border rounded py-1 px-3 bg-blue-400 text-white`} >
-                        <Link to={`/contracts/${props.id}`}>Contracts</Link>
-                    </div>
+                    <a className={`inline-block border rounded py-1 px-3 bg-blue-400 text-white`} href={`localhost:8000/V1/latest/${props.id}`}>
+                        Feed
+                    </a>
                 </div>
             </div>
         </div>
